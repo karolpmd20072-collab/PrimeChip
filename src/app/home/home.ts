46646e72568d61a5226d88ba3142b803165c523d
Karol Paganini <karolpmd20072@gmail.com>
2026-07-07 20:59:01 -0300

    att final

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -15,63 +16,63 @@ export class Home {
       nome: 'Processador Intel Core i5-13400F',
       antigo: 'R$ 1.499,00',
       novo: 'R$ 1.274,15',
-      imagem: 'assets/imagens/processador.png',
+      imagem: 'assets/../../assets/imagens/processador.png',
       alt: 'PROCESSADOR I5'
     },
     {
       nome: 'Placa de Vídeo RTX 4060 Ti',
       antigo: 'R$ 3.299,00',
       novo: 'R$ 2.804,15',
-      imagem: 'assets/imagens/placa 4060ti.jpg',
+      imagem: 'assets/../../assets/imagens/placa 4060ti.jpg',
       alt: 'PLACA DE VÍDEO'
     },
     {
       nome: 'Memória RAM DDR4 FURY 16GB 3200MHz',
       antigo: 'R$ 980,00',
       novo: 'R$ 833,00',
-      imagem: 'assets/imagens/memoria ran.jpg',
+      imagem: 'assets/../../assets/imagens/memoria ran.jpg',
       alt: 'MEMÓRIA RAM'
     },
     {
       nome: 'SSD Kingston NV2 1TB NVMe M.2',
       antigo: 'R$ 950,00',
       novo: 'R$ 807,50',
-      imagem: 'assets/imagens/memoria kingston.jpg',
+      imagem: 'assets/../../assets/imagens/memoria kingston.jpg',
       alt: 'SSD KINGSTON'
     },
     {
       nome: 'Fonte Corsair CV550 550W 80 Plus Bronze',
       antigo: 'R$ 400,00',
       novo: 'R$ 340,00',
-      imagem: 'assets/imagens/Fonte.png',
+      imagem: 'assets/../../assets/imagens/Fonte.png',
       alt: 'FONTE CORSAIR'
     },
     {
       nome: 'Water Cooler MasterLiquid 240mm',
       antigo: 'R$ 684,00',
       novo: 'R$ 581,40',
-      imagem: 'assets/imagens/Water cooler.png',
+      imagem: 'assets/../../assets/imagens/Water cooler.png',
       alt: 'WATER COOLER'
     },
     {
       nome: 'Gabinete Gamer ATX Vidro Temperado',
       antigo: 'R$ 399,00',
       novo: 'R$ 339,15',
-      imagem: 'assets/imagens/Gabinete.PNG',
+      imagem: 'assets/../../assets/imagens/Gabinete.PNG',
       alt: 'GABINETE'
     },
     {
       nome: 'Placa Mãe ASUS TUF Gaming B550M-Plus',
       antigo: 'R$ 793,49',
       novo: 'R$ 674,39',
-      imagem: 'assets/imagens/Placa Mae.png',
+      imagem: 'assets/../../assets/imagens/Placa Mae.png',
       alt: 'PLACA MÃE'
     },
     {
       nome: 'Processador AMD Ryzen 7 5700X',
       antigo: 'R$ 1.208,00',
       novo: 'R$ 1.026,80',
-      imagem: 'assets/imagens/Amd ryzen.png',
+      imagem: 'assets/../../assets/imagens/Amd ryzen.png',
       alt: 'AMD RYZEN'
     }
   ];
